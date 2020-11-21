1d98937d7f84dc611a6ec26bd94a627390886790
vova <vova.shkar74@gmail.com>
2020-11-21 21:19:25 +0300

    Add cycle and conditions, also results

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* jshint -W097 */ 
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посммотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,12 +11,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько вы его оцените?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько вы его оцените?', '');
+for (let i=0; i < numberOfFilms; i++) {
+    let a = '',
+        b = '';
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    while (a.length == 0 || a.length > 50) {
+        a = prompt('Один из последних просмотренных фильмов?', '');
+    }
+
+    while (b.length == 0 || b.length > 50) {
+        b = prompt('На сколько вы его оцените?', '');
+    }
+
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
+if (personalMovieDB.count < 10 && personalMovieDB.count != 0) {
+    alert('Просмотренно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30 ) {
+    alert('Вы киноман');
+} else { 
+    alert('Вы не смотрите фильмы');
+}
