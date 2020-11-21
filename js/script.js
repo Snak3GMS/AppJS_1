d85d897b62eb1a26580dd37ad22ab204d823c91f
vova <vova.shkar74@gmail.com>
2020-11-21 19:12:17 +0300

    add object DB, edit this

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 /* jshint -W097 */ 
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посммотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько вы его оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько вы его оцените?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
